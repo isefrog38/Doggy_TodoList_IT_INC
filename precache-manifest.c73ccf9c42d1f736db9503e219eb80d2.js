5f07140cbb3a0db4345cdb6fcdd15dc6ec09eadc
isefrog38 <isefroge38@gmail.com>
2022-02-15 00:27:46 +0300

    Updates

---
+++ precache-manifest.c73ccf9c42d1f736db9503e219eb80d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1c016541f91d42c7b02ec517e49185",
+    "url": "/Doggy_TodoList_IT_INC/index.html"
+  },
+  {
+    "revision": "ffbe3024258366df70b5",
+    "url": "/Doggy_TodoList_IT_INC/static/css/main.b453ea92.chunk.css"
+  },
+  {
+    "revision": "b321c45e5fca89349c28",
+    "url": "/Doggy_TodoList_IT_INC/static/js/2.81443b7e.chunk.js"
+  },
+  {
+    "revision": "b3019697dd82d502c1d7617184405246",
+    "url": "/Doggy_TodoList_IT_INC/static/js/2.81443b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbe3024258366df70b5",
+    "url": "/Doggy_TodoList_IT_INC/static/js/main.4515ff1c.chunk.js"
+  },
+  {
+    "revision": "508ea3c1276812f0cb94",
+    "url": "/Doggy_TodoList_IT_INC/static/js/runtime-main.7aab97cf.js"
+  },
+  {
+    "revision": "2bd405dab5c2396392cadba2d0c597bb",
+    "url": "/Doggy_TodoList_IT_INC/static/media/12.2bd405da.jpg"
+  }
+]);
